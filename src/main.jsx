8c46f12b67e13ddbea5c256b8412fda6b9946f6a
Noah Parsons <161479672+GuiloScion@users.noreply.github.com>
2025-06-29 08:53:02 -0400

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RocketSimulator from './RocketSimulator';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RocketSimulator />);
